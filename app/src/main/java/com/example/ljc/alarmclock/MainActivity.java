446fd9a48a869062ce66b5eb205b3744544440b1
liujinchao <liujinchao@gionee.com>
2017-10-18 18:24:21 +0800

    1018

--- app/src/main/java/com/example/ljc/alarmclock/MainActivity.java
+++ app/src/main/java/com/example/ljc/alarmclock/MainActivity.java
@@ -1,15 +1,20 @@
 package com.example.ljc.alarmclock;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Handler;
+import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.ljc.alarmclock.adapter.AlarmAdapter;
 import com.example.ljc.alarmclock.database.AlarmDataHelper;
@@ -44,11 +49,11 @@ public class MainActivity extends AppCompatActivity {
 
 
         Handler mTimeHandler = new Handler() {
-            public void handleMessage(android.os.Message msg) {
+            public void handleMessage(Message msg) {
                 if (msg.what == 0) {
-                    long time=System.currentTimeMillis();
-                    Date date=new Date(time);
-                    SimpleDateFormat sdformat=new SimpleDateFormat("yyyy年MM月dd日 \nHH时mm分ss秒 \nEEEE");
+                    long time = System.currentTimeMillis();
+                    Date date = new Date(time);
+                    SimpleDateFormat sdformat = new SimpleDateFormat("yyyy年MM月dd日 \nHH时mm分ss秒 \nEEEE");
                     ymdw.setText(sdformat.format(date));
                     sendEmptyMessageDelayed(0, 0);
                 }
@@ -70,10 +75,56 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        initListData();
-    }
+        listView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
+                new AlertDialog.Builder(MainActivity.this).setTitle("选项").setMessage("删除闹钟").setNegativeButton("取消",null).setPositiveButton("确定", new DialogInterface.OnClickListener() {
+                     @Override
+                   public void onClick(DialogInterface dialog, int which) {
+                            Toast.makeText(MainActivity.this, "点击了确定", Toast.LENGTH_SHORT).show();
+                     }
+                });)
+            })
 
-    public void initListData(){
+    });
+
+//    3         builder.setTitle("标题栏");
+// 24         builder.setMessage("正文部分，简单的文本");
+// 25         builder.setPositiveButton("确定",new DialogInterface.OnClickListener() {
+//        26             @Override
+//        27             public void onClick(DialogInterface dialog, int which) {
+//            28                 Toast.makeText(MainActivity.this, "点击了确定", Toast.LENGTH_SHORT).show();
+//            29             }
+//        30         });
+// 31         builder.setNegativeButton("取消",null);
+// 32         builder.setNeutralButton("中立",null);
+//        listView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {//长按删除闹钟
+//            @Override
+//            public boolean onItemLongClick(AdapterView<?> adapterView, View view, final int position, long l) {
+//                new AlertDialog.Builder(getContext())
+//                        .setTitle("操作选项")
+//                        .setItems(new CharSequence[]{"删除"}, new DialogInterface.OnClickListener() {
+//                            @Override
+//                            public void onClick(DialogInterface dialogInterface, int i) {
+//                                switch (i) {
+//                                    case 0:
+//                                        deleteAlarm(position);
+//                                        break;
+//                                    default:
+//                                        break;
+//                                }
+//                            }
+//                        }).setNegativeButton("取消", null)
+//                        .show();
+//                return true;
+//            }
+//        });
+
+    initListData();
+
+}
+
+    public void initListData() {
 //
 //        public Alarm(int hour, int minutes, int daysofweek, boolean vibrate, boolean ring, boolean state) {
 //            this.hour = hour;
@@ -85,7 +136,7 @@ public class MainActivity extends AppCompatActivity {
 //        }
 
         SQLiteDatabase db = dbHelper.getWritableDatabase();
-        Cursor cursor = db.query("alarms", null, null, null, null,null, null);
+        Cursor cursor = db.query("alarms", null, null, null, null, null, null);
 
 //        db.execSQL("CREATE TABLE alarms (" +
 //                "_id INTEGER PRIMARY KEY," +
@@ -96,7 +147,7 @@ public class MainActivity extends AppCompatActivity {
 //                "ring INTEGER, " +
 //                "state INTEGER);");
 
-        if (cursor.moveToNext()){
+        if (cursor.moveToNext()) {
             do {
                 int hour = cursor.getInt(cursor.getColumnIndex("hour"));
                 int minutes = cursor.getInt(cursor.getColumnIndex("minutes"));
@@ -104,9 +155,9 @@ public class MainActivity extends AppCompatActivity {
                 int vibrate = cursor.getInt(cursor.getColumnIndex("vibrate"));
                 int ring = cursor.getInt(cursor.getColumnIndex("ring"));
                 int state = cursor.getInt(cursor.getColumnIndex("state"));
-                Alarm alarm = new Alarm(hour, minutes, daysofweek, vibrate==1?true:false, ring==1?true:false, state==1?true:false);
+                Alarm alarm = new Alarm(hour, minutes, daysofweek, vibrate == 1 ? true : false, ring == 1 ? true : false, state == 1 ? true : false);
                 alarmList.add(alarm);
-            }while (cursor.moveToNext());
+            } while (cursor.moveToNext());
         }
         cursor.close();
         alarmAdapter = new AlarmAdapter(this, alarmList);
